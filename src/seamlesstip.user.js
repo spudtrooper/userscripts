96f9237fb301a07483422c094586e8923cfdfcfb
Jeffrey Palm <jeff@jeffpalm.com>
2012-08-07 17:54:26 -0400

    added seamless userscript

--- src/seamlesstip.user.js
+++ src/seamlesstip.user.js
@@ -7,12 +7,32 @@
 
 (function() {
 
+  function useWeather(data) {
+    console.log('data');
+    console.log(data);
+  }
+
   function useLocation(pos) {
     var c = pos.coords;
     var lat = c.latitude;
     var lng = c.longitude;
-    console.log('lat:' + lat);
-    console.log('lng:' + lng);
+    
+    // Look up weather
+    var url = 
+      'http://api.wunderground.com/api/b96d9f3b9d9ad40c/geolookup/q/'
+      + lat + ',' + lng + '.json';
+    ajax(url,useWeather);
+  }
+
+  function ajax(url,f) {
+    var xmlHttp = new XMLHttpRequest(); 
+    xmlHttp.onreadystatechange = function () { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) { 
+	f.call(this,xmlHttp.responseText); 
+      } 
+    }; 	
+    xmlHttp.open('GET',url,true);
+    xmlHttp.send(null);
   }
 
   function errorLocation(error) {
@@ -21,6 +41,7 @@
 
   function main() {
     
+    // Find location
     var geo = navigator.geolocation;
     if (geo) {
       geo.getCurrentPosition(useLocation, errorLocation, {
