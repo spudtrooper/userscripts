7a2859c5a21c0d26b5083a3eb1a5a2ffab9530fd
spudtrooper <jeff@jeffpalm.com>
2012-12-30 15:42:11 -0500

    dot space space

---
+++ src/dotSpaceSpace.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name          Dot space space
+// @namespace     http://jeffpalm.com/dotspacespace/
+// @description   Alerts when you type two spaces after a period.
+// @include       http://*
+// @include       https://*
+// ==/UserScript==
+
+(function() {
+
+  const KEY_DOT = 190;
+  const KEY_SPACE = 32;
+  const DOT_SPACE_SPACE_MASK = (KEY_SPACE << 16) | (KEY_SPACE << 8) | KEY_DOT;
+
+  var buf = 0;
+
+  function keyPress(e) {
+    buf >>= 8;
+    buf |= (e.keyCode % 0xff) << 16;
+    if (buf == DOT_SPACE_SPACE_MASK) {
+      alert('Don\'t type two spaces after a period!');
+      buf = 0;
+    }
+  }
+  
+  function main() {
+    window.addEventListener('keydown',keyPress,true);
+  }
+
+  main();
+
+})();
